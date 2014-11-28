8d86201ba28a9eb8f1e19d9380ad9ae1066818d2
Martin Cruz <slayer.dmx@gmail.com>
2014-11-27 23:38:12 -0500

    Create .gitignore

---
+++ src/expresionesregulares/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package expresionesregulares;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ *
+ * @author martin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ExpresionRegularFrame er = new ExpresionRegularFrame();
+        er.setLocationRelativeTo(null);
+        er.setVisible(true);
+        
+    }
+    
+}
